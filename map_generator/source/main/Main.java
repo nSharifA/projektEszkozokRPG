3517f43780ab0781f266130e092afa5d2005864c
nSharifA <n.sharif@windowslive.com>
2019-04-28 16:03:48 +0200

    findbugs working version

--- map_generator/source/main/Main.java
+++ map_generator/source/main/Main.java
@@ -1,7 +1,7 @@
 package main;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.*;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -23,10 +23,10 @@ public class Main {
 	}
 
 	public static void runFromCommandLine(String[] args) throws IOException {
-		int wTileCount = Integer.valueOf(args[0]);
-		int hTileCount = Integer.valueOf(args[1]);
-		int freq = Integer.valueOf(args[2]);
-		int seed = Integer.valueOf(args[3]);
+		Integer wTileCount = Integer.valueOf(args[0]);
+		Integer hTileCount = Integer.valueOf(args[1]);
+		Integer freq = Integer.valueOf(args[2]);
+		Integer seed = Integer.valueOf(args[3]);
 		if (wTileCount > 1000 || wTileCount < 20 || hTileCount > 1000 || hTileCount < 20 || freq < 30 || freq > 100
 				|| seed < 0) {
 			throw new IOException();
@@ -35,7 +35,9 @@ public class Main {
 		BufferedImage noiseImage = gen.getNoiseImage(false, true);
 		if (noiseImage != null) {
 			File outputfile = new File("image_seed_" + seed + "freq_" + freq + ".jpg");
-			ImageIO.write(noiseImage, "jpg", outputfile);
+			Writer w = new OutputStreamWriter(new FileOutputStream(outputfile), "UTF-8");
+			PrintWriter pw = new PrintWriter(w);
+			pw.close();
 		}
 	}
 }
